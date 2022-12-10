660359225db42b7f4b383030db2b2b080f569be6
Carlos Lapao <cjlapao@gmail.com>
2022-12-10 07:54:11 +0000

    Added starting of unit tests

--- main_test.go
+++ main_test.go
@@ -3,7 +3,13 @@ package redisclient
 import (
 	"reflect"
 	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis"
+	"github.com/stretchr/testify/assert"
 )
+
+var redisServer *miniredis.Miniredis
 
 func TestRedisClient_parseConnectionString(t *testing.T) {
 	type args struct {
@@ -101,4 +107,102 @@ func TestRedisClient_parseConnectionString(t *testing.T) {
 			}
 		})
 	}
+}
+
+func mockRedis() *miniredis.Miniredis {
+	s, err := miniredis.Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
+func setup() *RedisClient {
+	redisServer = mockRedis()
+
+	return New(redisServer.Addr())
+}
+
+func teardown() {
+	redisServer.Close()
+}
+
+func TestRedisClient_Set(t *testing.T) {
+	client := setup()
+
+	defer teardown()
+
+	err := client.SetString("foo", "bar")
+
+	assert.Nilf(t, err, "we expect the error to be nil")
+}
+
+func TestRedisClient_SetExpiring(t *testing.T) {
+	client := setup()
+
+	defer teardown()
+
+	setErr := client.SetExpiringString("foo", "bar", 1*time.Second)
+	value, getErr := client.GetStringKey("foo")
+	expiresIn := redisServer.TTL("foo")
+
+	assert.Nilf(t, setErr, "we expect the error to be nil")
+	assert.Nilf(t, getErr, "we expect the get error to be nil")
+	assert.Equalf(t, "bar", value, "we expected 'bar' found %s", value)
+	assert.Equalf(t, 1*time.Second, expiresIn, "we expect the message ttl to be 1 second")
+}
+
+func TestRedisClient_Get(t *testing.T) {
+	client := setup()
+
+	defer teardown()
+
+	setErr := client.SetString("foo", "bar")
+	result, getErr := client.GetStringKey("foo")
+
+	assert.Nilf(t, setErr, "we expect the set error to be nil")
+	assert.Nilf(t, getErr, "we expect the get error to be nil")
+	assert.Equalf(t, "bar", result, "expected 'bar' found %s", result)
+}
+
+func TestRedisClient_GetEmpty(t *testing.T) {
+	client := setup()
+
+	defer teardown()
+
+	result, getErr := client.GetStringKey("foo")
+
+	assert.Errorf(t, getErr, "the key does not exist", "error is not what expected")
+	assert.Equalf(t, "", result, "expected '' found %s", result)
+}
+
+func TestRedisClient_GetServerError(t *testing.T) {
+	client := setup()
+	teardown()
+
+	result, getErr := client.GetStringKey("foo")
+
+	assert.Errorf(t, getErr, "the key does not exist", "error is not what expected")
+	assert.Equalf(t, "", result, "expected '' found %s", result)
+}
+
+func TestRedisClient_Delete(t *testing.T) {
+	client := setup()
+
+	defer teardown()
+
+	setErr := client.SetString("foo", "bar")
+	result, getErr := client.GetStringKey("foo")
+	count, delErr := client.Delete("foo")
+	resultDelete, getDeleteErr := client.GetStringKey("foo")
+
+	assert.Nilf(t, setErr, "we expect the set error to be nil")
+	assert.Nilf(t, getErr, "we expect the get error to be nil")
+	assert.Equalf(t, "bar", result, "expected 'bar' found %s", result)
+	assert.Nilf(t, delErr, "we expect the get error to be nil")
+	assert.Equalf(t, int64(1), count, "expected count to be 1")
+	assert.Equalf(t, "", resultDelete, "expected the key to be empty after delete")
+	assert.Errorf(t, getDeleteErr, "we expect to have an error as key no longer exists")
 }
